e921cacde998f32470b2a50069407fc8cd2e7a0e
elisa239 <elisa.winograd@marleyspoon.com>
2016-09-11 16:26:29 -0500

    temporary version

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#slides").slidesjs({
+    pagination: {
+      active: false,
+        // [boolean] Create pagination items.
+        // You cannot use your own pagination. Sorry.
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    }
+  });
+});

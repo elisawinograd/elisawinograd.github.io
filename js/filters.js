da88fca28b4889075825a7c93874054a4253c8c1
elisawinograd <elisa.winograd@marleyspoon.com>
2017-09-29 13:47:44 +0200

    finished filters for projects

--- js/filters.js
+++ js/filters.js
@@ -1,10 +1,21 @@
 $( document ).ready(function() {
-	var $btns = $('.bttn').click(function() {
-	  if (this.id == 'all') {
+	var $btns1 = $('.bttn-blog').click(function() {
+	  if (this.id == 'all-blog') {
 	    $('#content-blog > div').fadeIn(450);
 	  } else {
 	    var $el = $('.' + this.id).fadeIn(450);
 	    $('#content-blog > div').not($el).hide();
+	  }
+	  $btns.removeClass('active');
+	  $(this).addClass('active');
+	});
+
+	var $btns2 = $('.bttn-projects').click(function() {
+	  if (this.id == 'all-projects') {
+	    $('#content-projects > div').fadeIn(450);
+	  } else {
+	    var $el = $('.' + this.id).fadeIn(450);
+	    $('#content-projects > div').not($el).hide();
 	  }
 	  $btns.removeClass('active');
 	  $(this).addClass('active');

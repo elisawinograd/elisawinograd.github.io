780851ce2590d812f028b03fee98c7ae958193d9
elisa winograd <elisa.wino@gmail.com>
2020-04-14 13:57:24 +0200

    new about and design pages

---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<body>
+<a href="index.html" style="text-decoration:none">
+				<div id="title">
+					<p class="title"> Elisa Winograd </p> 
+					<!--<p class="sub-title"> Creative Designer / Sustainability Enthusiast </p> -->
+				</div>
+			</a>
+			<div id="links">
+				<a class="nav-link" href="about.html"> about </a>
+				<a class="nav-link" href="index.html"> designs </a>
+				<a class="nav-link" href="blog.html"> blog </a>
+			</div>
+			</body>
+</html>
